16e8b75652824c743ba6b8e6d10f5f949bc03f2d
Константин Сацуро <Disney.mous18@mail.ru>
2026-04-21 12:32:53 +0300

    Add docker build

---
+++ backend/api/tests.py
@@ -0,0 +1,26 @@
+"""Модуль тестов для API приложения api."""
+
+from http import HTTPStatus
+
+from api import models
+from django.test import Client, TestCase
+
+
+class TaskiAPITestCase(TestCase):
+    """Тестовый класс для проверки API эндпоинтов задач."""
+
+    def setUp(self):
+        """Настройка тестового окружения перед каждым тестом."""
+        self.guest_client = Client()
+
+    def test_list_exists(self):
+        """Проверка доступности списка задач."""
+        response = self.guest_client.get('/api/tasks/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_task_creation(self):
+        """Проверка создания задачи."""
+        data = {'title': 'Test', 'description': 'Test'}
+        response = self.guest_client.post('/api/tasks/', data=data)
+        self.assertEqual(response.status_code, HTTPStatus.CREATED)
+        self.assertTrue(models.Task.objects.filter(title='Test').exists())
